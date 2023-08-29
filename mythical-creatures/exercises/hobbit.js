d4b56c2a803ba3f7e6f53acf2f9bf57b2ae28435
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-28 20:32:50 -0600

    Complete createHobbit function

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,7 +1,18 @@
+//Create a function createHobbit with two parameters : name and age
+  //create a variable hobbitObj Object and assign it key-value pairs of name and age. Assign the name and age the values of the arguments
+  //passed in. If there isn't anything passed in to name, assign it the value of 'unknown', and if there isn't an age passed in, assign it the value
+  //of zero
+function createHobbit(name = 'unknown', age = 0){
+  hobbitObj = {
+    name : name,
+    age : age
+  }
+  return hobbitObj;
+}
 
 
 module.exports = {
-  // createHobbit, 
+  createHobbit, 
   // celebrateBirthday, 
   // getRing, 
   // meetPeople, 
