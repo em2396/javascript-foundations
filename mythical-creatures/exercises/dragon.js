97306d6beadee029dee60d67d907f640e83c1a3c
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-28 20:25:22 -0600

    Complete findFireBreathers function

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -7,7 +7,9 @@ function createDragon(dragonsName, riderName, temperment){
   var newDragon = {
     name : dragonsName,
     rider : riderName,
-    temperment: temperment
+    temperment: temperment,
+    timesEaten : 0,
+    hungry : true
     }
   return newDragon
 }
@@ -20,19 +22,51 @@ function greetRider(dragon){
 
 //Create a function eat 
   //Add two key-value pairs, one to be a boolean of whether the dragon has eaten or not, and the other being the amount of times eaten
+  //Need to add to the createDragon function; 
 function eat(dragonObj){
-  dragonObj.hungry = true;
-  dragonObj.timesEaten = 0;
+  dragonObj.timesEaten++
+  if(dragonObj['timesEaten'] >= 3){
+    dragonObj['hungry'] = false;
+  }
   return dragonObj
 }
 
-createDragon('Mushu', 'Mulan', 'calm')
-console.log('Mushu');
+var mushu = createDragon('mushu', 'mulan', 'calm')
+//console.log(mushu);
+//console.log(eat(mushu));
+var eragon = createDragon('eragon', 'em', 'aggressive');
 
+
+//Create a function find fireBreathers with an unknown amount of parameters ...dragonObj
+  //create a new variable fireBreathers and assign it the value of an empty array
+  //iterate through the dragonObj
+    //if the dragonObj.temperment equals 'aggressive', push the index into fireBreathers
+    //return fireBreathers
+
+  function findFireBreathers(dragonArr){
+    var fireBreather = [];
+    for(let i = 0; i < dragonArr.length; i++){ //to iterate over the array of objects
+      //console.log(dragonObj[i]) - the dragon object
+      if(dragonArr[i].temperment === 'aggressive'){
+        fireBreather.push(dragonArr[i])
+      }
+    }
+    return fireBreather;
+  }
+  
+  var dragon1 = createDragon('Gray', 'Marley', 'aggressive');
+  var dragon2 = createDragon('Sky', 'Susie', 'gentle');
+  var dragon3 = createDragon('Mushu', 'Mulan', 'aggressive');
+  var dragon4 = createDragon('Lady Vox', 'Emily', 'gentle');
+
+  var allDragons = [dragon1, dragon2, dragon3, dragon4];
+  
+  var fireBreathers = findFireBreathers(allDragons);
+  console.log(fireBreathers)
 
 module.exports = {
   createDragon, 
   greetRider, 
-  // eat, 
-  // findFireBreathers
+  eat, 
+  findFireBreathers
 }
