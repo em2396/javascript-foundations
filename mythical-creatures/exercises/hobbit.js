e8d09bda8f47de1e479091579a5622f5e86c7def
Emalee Poellot <emalee.poellot@gmail.com>
2023-08-30 13:31:05 -0600

    Complete celebrateBirthday function

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,37 +3,52 @@
   //passed in. If there isn't anything passed in to name, assign it the value of 'unknown', and if there isn't an age passed in, assign it the value
   //of zero
 function createHobbit(name = 'unknown', age = 0){
-  hobbitObj = {
+  var hobbitObj = {
     name : name,
     age : age,
+    isAdult: false,
+    isOld : false
   }
-  return hobbitObj;
-}
+    return hobbitObj;
+  }
+
 
 //Create a function celebrateBirthday with one parameter: hobbitObj
   //increment hobbitObj.age by one after every year (every time the function is called)
   //if age is 32 or younger, hobbit isn't an adult (add a isAdult variable to obj)
 
-function celebrateBirthday(hobbitObj){
-  //console.log(hobbitObj.age)
- // hobbitObj.age++;
-  if(hobbitObj.age <= 32){
-    hobbitObj.isAdult = false;
-    hobbitObj['age']++
-    return hobbitObj;
-  } else if(hobbitObj.age > 32){
+// function celebrateBirthday(hobbitObj){
+//   //console.log(hobbitObj.age)
+//   if(hobbitObj.age >= 101){
+//     hobbitObj.age++
+//     hobbitObj['isOld'] = true;
+//     //return hobbitObj;
+//   } else {
+//     hobbitObj.age++
+//     hobbitObj['isOld'] = false;
+//     //return hobbitObj;
+//   }
+//   return hobbitObj;
+// }
+
+function celebrateBirthday(hobbitObj) {
+  hobbitObj.age++;
+  if(hobbitObj.age >= 33){
     hobbitObj.isAdult = true;
-    return hobbitObj;
   }
+  if(hobbitObj.age >= 101){
+    hobbitObj.isOld = true; 
+  }
+  return hobbitObj;
 }
 
-var hobbit = createHobbit('Meriadoc');
-console.log(hobbit)
-var olderHobbit = celebrateBirthday(hobbit);
-var evenOlderHobbit = celebrateBirthday(olderHobbit);
-var evenOlderStillHobbit = celebrateBirthday(evenOlderHobbit);
-console.log(olderHobbit);
-console.log(evenOlderHobbit);
+//var hobbit = createHobbit('Meriadoc');
+//console.log(hobbit)
+//var olderHobbit = celebrateBirthday(hobbit);
+//var evenOlderHobbit = celebrateBirthday(olderHobbit);
+//var evenOlderStillHobbit = celebrateBirthday(evenOlderHobbit);
+//console.log(olderHobbit);
+//console.log(evenOlderHobbit);
 
 module.exports = {
   createHobbit, 
