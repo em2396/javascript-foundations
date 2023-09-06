225906b0cd138a2221f37c6d478a217121254542
Emalee Poellot <emalee.poellot@gmail.com>
2023-09-05 21:12:31 -1000

    Complete most functions, start on calculatePrice function

--- tacoStand/tacoStand.js
+++ tacoStand/tacoStand.js
@@ -4,17 +4,33 @@ function createIngredient(name = 'unknown', price = 0.00){
         price : price
     }
     return ingredient;
+};
+
+function createTaco(name = 'custom', ingredients = []){
+   var tacoName = {
+    name : name,
+    ingredients : ingredients
+   }
+   return tacoName;
+};
+
+function addIngredientToTaco(tacoName, ingredient) {
+    tacoName.ingredients.push(ingredient);
+    return tacoName;
 }
-
-function createTaco(name = 'custom'){
-
+//price = tacoName.ingredients.ingredient.price
+function calculatePrice(tacoName, price) {
+    var totalCost = 0;
+    console.log(tacoName.ingredients.price)
+    for (var i = 0; i < tacoName.ingredients.length; i++){
+    tacoName.ingredients[i].price+=totalCost
+    }
+    return tacoName.name, totalCost;
 }
-
-
 
 module.exports = {
     createIngredient,
-    createTaco
-    //addIngredientsToTaco,
-    //calculatePrice
+    createTaco,
+    addIngredientToTaco,
+    calculatePrice
 }
