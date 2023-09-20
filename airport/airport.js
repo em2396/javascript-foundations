cb4d5315c4ba53fdfa940667a874464d9d1dad67
Emalee Poellot <emalee.poellot@gmail.com>
2023-09-19 14:22:52 -1000

    Get to landPlanes function

--- airport/airport.js
+++ airport/airport.js
@@ -1,9 +1,32 @@
+function createAirport(name, airlines=[], gates) {
+  return {
+    name: name,
+    availableGates: gates,
+    airlines: airlines
+  }
+};
 
+function welcomeGuests(airportName) {
+  return `Welcome to ${airportName.name}!`
+}
+
+function landPlanes(airportNameObj, numOfPlanes) {
+  // var difference = airportNameObj.availableGates - numOfPlanes
+  if (airportNameObj.availableGates >= 1) {
+    airportNameObj.availableGates -= numOfPlanes;
+    airportNameObj.message = `Success! Current availability is ${airportNameObj.availableGates}.`
+  } else {
+    airportNameObj.availableGates = 0;
+    airportNameObj.message = `Oh no! Not enough gates available. Current overflow is ${Math.abs(airportNameObj.availableGates - numOfPlanes)}.`
+    console.log(airportNameObj.message);
+  }
+  return airportNameObj;
+}
 
 
 module.exports = { 
-  // createAirport, 
-  // welcomeGuests, 
-  // landPlanes, 
+  createAirport, 
+  welcomeGuests, 
+  landPlanes, 
   // checkAirlineLocations
 };
